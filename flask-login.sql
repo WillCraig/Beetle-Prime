d4f6b118add34f3668276376dde5a60a6c32fa05
William Craig <wdcraig99@gmail.com>
2023-11-17 18:35:07 -0500

    adding to gitignore

--- flask-login.sql
+++ flask-login.sql
@@ -1,3 +1,9 @@
+
+
+CREATE DATABASE IF NOT EXISTS `testDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `testDB`;
+
+
 CREATE TABLE IF NOT EXISTS `accounts` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
         `username` varchar(50) NOT NULL,

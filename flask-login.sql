e3af6e3aaf1785798d2eab84a8c1f05d57376259
William Craig <wdcraig99@gmail.com>
2023-11-17 14:07:29 -0500

    Adding bill reed's code

---
+++ flask-login.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `accounts` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `username` varchar(50) NOT NULL,
+        `password` varchar(255) NOT NULL,
+        `email` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;

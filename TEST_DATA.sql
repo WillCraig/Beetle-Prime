2c3ea31fe6c0d2d08284fd003de092762dfbe891
Dylan Sarell <116117025+dsarell1@users.noreply.github.com>
2023-12-05 13:16:24 -0500

    Updated Products the images should work. They Work for me.

--- TEST_DATA.sql
+++ TEST_DATA.sql
@@ -1,43 +1,53 @@
 # test customer
 INSERT INTO customer (username, password, email, street, city, state, zipcode) VALUES
-('s', 's', 'john.doe@email.com', '123 Oak St', 'Cityville', 'Stateville', 54321);
+('John Doe', 's', 'john.doe@email.com', '123 Oak St', 'Cityville', 'Stateville', 54321);
 
 
 # SELLER
-INSERT INTO seller (name, password, email, street, city, state, zipcode) VALUES ('Beedle', 's', 'email@email.com', 'street', 'city', 'state', 12345);
+INSERT INTO seller (name, password, email, street, city, state, zipcode) VALUES ('Beedle', 's', 'beedle@email.com', 'Hyrule Castle Town Rd', 'Hyrule Castle Town', 'Central Hyrule', 12300);
 
-INSERT INTO seller (name, password, email, street, city, state, zipcode) VALUES ('Link', 's', 'email@email.com', 'street', 'city', 'state', 12345);
+INSERT INTO seller (name, password, email, street, city, state, zipcode) VALUES ('Link', 's', 'Link@email.com', 'Hateno Rd', 'Hateno Village', 'East Neculda', 24341);
 
-INSERT INTO seller (name, password, email, street, city, state, zipcode) VALUES ('Mario', 's', 'email@email.com', 'street', 'city', 'state', 12345);
+INSERT INTO seller (name, password, email, street, city, state, zipcode) VALUES ('Mario', 's', 'Mario@email.com', 'Mushroom Kingdom Drive', 'Broklyn', 'New York', 12345);
 
 
 # PRODUCT
 # Beedle
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (1, 'Heart Potion', 'Heals 8 hearts when consumed', 19.99, 1, 'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/0/07/SS_Heart_Potion_Model.png/revision/latest/scale-to-width-down/183?cb=20210815143730&format=original');
+VALUES (1, 'GreenLand', 'Literary Greenland', 500000.00, 1, 'https://www.climate.gov/sites/default/files/styles/full_width_620_alternate_image/public/MODISalbedo_greenland2011_610.jpg?itok=l0K2ADs9');
 
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (1, 'Bundle of Arrows', 'A bundle of 10 arrows', 7.99, 10, 'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/9/97/OoT3D_Bundle_of_Arrows_Model_2.png/revision/latest?cb=20200902122040&format=original');
+VALUES (1, 'Arrows', 'Use These Handy Things With a Bow', 1.00, 200, 'https://cdn1.iconfinder.com/data/icons/arrows-409/64/Arrow_Up_Arrow-12-512.png');
 
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (1, 'Quiver', 'Increases the amount of arrows you can carry', 39.99, 1, 'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/2/24/TWWHD_Quiver_Upgrade_1_Artwork.png/revision/latest/scale-to-width-down/320?cb=20130911215147&format=original');
+VALUES (1, 'Rugged Rhino Beetle', 'My Precious', 79999.99, 1, 'https://cdn.wikimg.net/en/zeldawiki/images/a/a0/TotK_Rugged_Rhino_Beetle_Model.png');
+
+INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
+VALUES (1, 'Super Mario bros. Movie', 'The Orignal 1993 Movie now on DVD', 20.00, 50, 'https://upload.wikimedia.org/wikipedia/en/thumb/9/94/SMB_Movie_Poster.jpg/220px-SMB_Movie_Poster.jpg');
+
+INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
+VALUES (1, 'Sony Walkman', 'The Mp3 player Before it was cool', 69.99, 126, 'https://images.reverb.com/image/upload/s--UHPwFyIy--/a_0/f_auto,t_large/v1672033355/lifevpkokxxlamllyne5.jpg');
+
 
 # Link
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (2, 'Hookshot', 'Can be shot and pull you at high speeds', 149.99, 1, 'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/f/f8/TWWHD_Hookshot_Artwork.png/revision/latest/scale-to-width-down/312?cb=20130911220003&format=original');
+VALUES (2, 'Jar of Dirt', 'This Jar will Capture your Heart', 47.25, 58, 'https://i.kym-cdn.com/entries/icons/facebook/000/002/914/jar-of-dirt.jpg');
 
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (2, 'Bow', 'Can be used to shoot arrows', 74.99, 1, 'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/b/bb/TWWHD_Hero%27s_Bow_Artwork.png/revision/latest/scale-to-width-down/320?cb=20130914155019&format=original');
+VALUES (2, 'Havoc in Hyrule', 'Vintage Legend of Zelda DVD.', 20.99, 30, 'https://m.media-amazon.com/images/I/51TKYxnXFYL._AC_UF894,1000_QL80_.jpg');
 
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (2, 'Fairy in a Bottle', 'The fairy will heal you and save you from dying', 99.99, 1, 'https://static.wikia.nocookie.net/zelda_gamepedia_en/images/6/67/SSHD_Fairy_Render.png/revision/latest/scale-to-width-down/178?cb=20220308175755&format=original');
+VALUES (2, 'Fanny Pack', 'A tool Belt for your needs', 12.50, 89, 'https://chromeindustries.com/cdn/shop/products/1_BG-288-BK.jpg?v=1701459692&width=2048');
 
 # Mario
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (3, 'Mushroom', 'Increases your size, strength, and life', 14.99, 1, 'https://static.wikia.nocookie.net/nintendo/images/5/5b/Mushroom1.jpg/revision/latest?cb=20111104224030&path-prefix=en');
+VALUES (3, 'Mario Cap', 'From it\'s a Me! Please Ingnore the Eyes', 199.99, 4, 'https://m.media-amazon.com/images/I/8127FzX1QxS._AC_UY1000_.jpg');
 
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (3, 'Fire Flower', 'Increases size and life as well as fireball powers', 29.99, 1, 'https://static.wikia.nocookie.net/nintendo/images/5/55/MK8_Fire_Flower.png/revision/latest/scale-to-width-down/1000?cb=20170206125257&path-prefix=en');
+VALUES (3, 'Pac-Man Lunch Box', 'Vintage Pac-man themed Lunch box.', 9.99, 23, 'https://i.ebayimg.com/images/g/E~gAAOSw6jdjdnoa/s-l1600.jpg');
 
 INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
-VALUES (3, 'Starman', 'Grants you temporary invincibility', 49.99, 1, 'https://static.wikia.nocookie.net/nintendo/images/2/2e/Star.png/revision/latest?cb=20150501195537&path-prefix=en');
+VALUES (3, 'Shaq Fu!', 'Easily the greatest game of All time', 102.34, 11, 'https://i.ebayimg.com/images/g/KYQAAMXQya1Q5ySQ/s-l600.jpg');
+
+INSERT INTO product (seller_id, name, description, price, product_quantity, img_link)
+VALUES (3, 'Garfield Plush', 'Chris Pratt in marketible Plushie Form', 300.15, 7, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDHspOS6n6xlDIWlBqLu4JTyDKC_T_765-A&usqp=CAU');
